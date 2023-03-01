463599f315b0546a91d201e7ddfd00788065c418
lavkeshchhatani <lavkeshchhatani@gmail.com>
2023-03-02 01:13:39 +0530

    creating directory component and category-item component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import './category-item.styles.scss'
+
+export default function CategoryItem({category}) {
+  return (
+    <div  className="category-container">
+            <div
+              className="background-image"
+              style={{ backgroundImage: `url(${category.imageUrl})` }}
+            >
+            </div>
+              <div className="category-body-container">
+                <h2>{category.title}</h2>
+                <p>Shop Now</p>
+            </div>
+          </div>
+  )
+}
